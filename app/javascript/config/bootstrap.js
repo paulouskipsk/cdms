e74d16303cd725ada7745a4b5c47c475070bf26b
Diego Marczal <dmarczal@gmail.com>
2020-08-18 11:42:31 -0300

    Setup eslint

--- app/javascript/config/bootstrap.js
+++ app/javascript/config/bootstrap.js
@@ -1,5 +1,5 @@
-import 'bootstrap'
+import 'bootstrap';
 
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip({container: '#main-content'})
-})
+document.addEventListener('turbolinks:load', () => {
+  $('[data-toggle="tooltip"]').tooltip({ container: '#main-content' });
+});
