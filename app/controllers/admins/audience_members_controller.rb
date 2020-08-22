a4b1c24240ab1c3236dce34f13965701b0134ba8
paulouskipsk <paulouskipsk@gmail.com>
2020-08-22 17:58:27 -0300

    Add page to edit audience_member

--- app/controllers/admins/audience_members_controller.rb
+++ app/controllers/admins/audience_members_controller.rb
@@ -22,8 +22,28 @@ class Admins::AudienceMembersController < ActionController::Base
     end
   end
 
+  def edit 
+    @audience_member = AudienceMember.find(params[:id])
+  end
+
+  def update 
+    audience_member = AudienceMember.find(audience_member_params[:id])
+    audience_member.name = audience_member_params[:name]
+    audience_member.email = audience_member_params[:email]
+    audience_member.cpf = audience_member_params[:cpf]
+
+    if audience_member.valid?
+        audience_member.save
+        flash[:success] = I18n.t('flash.actions.update.m', { resource_name: I18n.t('activerecord.models.audience_member.one') })
+        redirect_to list_audience_members_path        
+    else
+      @audience_member = audience_member
+      render :edit
+    end
+  end
+
   private
     def audience_member_params
-      params.require(:audience_member).permit(:name, :email, :cpf)
+      params.require(:audience_member).permit(:id,:name, :email, :cpf)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,10 @@ Rails.application.routes.draw do
     put 'admins/edit', to: 'admins/registrations#update', as: 'admin_registration'
 
     get '/audience_members', to: 'admins/audience_members#index', as: 'list_audience_members'
+    get '/audience_members/:id', to: 'admins/audience_members#edit', as: 'edit_audience_member'
     get '/audience_members/new', to: 'admins/audience_members#new', as: 'new_audience_member'
     post '/audience_members', to: 'admins/audience_members#create', as: 'create_audience_member'
+    patch '/audience_members', to: 'admins/audience_members#update', as: 'update_audience_member'
+
   end
 end
