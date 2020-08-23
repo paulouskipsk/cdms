71d82498edf256e3252e79a767ffae27556bb28e
paulouskipsk <paulouskipsk@gmail.com>
2020-08-23 10:51:53 -0300

    Add js mask to inputs

--- app/javascript/packs/admins/application.js
+++ app/javascript/packs/admins/application.js
@@ -2,3 +2,16 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 
 require('config/bootstrap');
+require('jquery-mask-plugin');
+
+
+$(document).ready(function(){
+   const inputs = $('[data-mask]').toArray();
+
+   
+   inputs.forEach(element => {
+       const mask = element.getAttribute('data-mask');
+       $(element).mask(mask);
+   });
+    
+});
