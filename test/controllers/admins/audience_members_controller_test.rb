be75b1303ff1774dbfcfadbe21010a2f5f741884
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-08-23 21:01:52 -0300

    Add controller tests

--- test/controllers/admins/audience_members_controller_test.rb
+++ test/controllers/admins/audience_members_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class Admins::AudienceMembersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'not logged in' do
+    should 'redirect to sing_in' do
+      get admins_list_audience_members_path
+      assert_response :redirect
+      assert_redirected_to new_admin_session_url
+    end
+  end
+
+  context 'logged in' do
+    should 'get admin dashboard' do
+      sign_in create(:admin)
+
+      get admins_list_audience_members_path
+      assert_response :success
+    end
+  end
 end
