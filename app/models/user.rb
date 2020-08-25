2500ce6e764a690016af20a5908542f79ec4d3a8
Matheus Mazepa <mazepa@MacBook-Pro-de-Matheus.local>
2020-08-24 21:23:11 -0300

    Add validatios unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :register_number, presence: true
-  validates :cpf, presence: true
+  validates :cpf, presence: true, uniqueness: true
   validates :status, inclusion: [true, false]
+
+  before_save do
+    self.email = self.username + "@utfpr.edu.br"
+  end
 end
