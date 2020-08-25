612447960fe350fd41882c790a7136e853918052
Matheus Mazepa <mazepa@MacBook-Pro-de-Matheus.local>
2020-08-25 19:21:44 -0300

    change quotes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true, uniqueness: true, format: { with: /\A[a-z0-9]+\Z/ }
   validates :register_number, presence: true
-  validates :cpf, presence: true, uniqueness: true
+  validates :cpf, presence: true
   validates :status, inclusion: [true, false]
 
   before_save do
-    self.email = self.username + "@utfpr.edu.br"
+    self.email = self.username + '@utfpr.edu.br'
   end
 end
