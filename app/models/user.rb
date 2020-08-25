f2e12357aeacc268479831a2dd5d7825bf886c63
Matheus Mazepa <mazepa@MacBook-Pro-de-Matheus.local>
2020-08-25 15:37:06 -0300

    Add translations missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validates :register_number, presence: true
   validates :cpf, presence: true, uniqueness: true
   validates :status, inclusion: [true, false]
