3abf605e2fd0332e68bf35e4b416f51e280d2cac
Matheus Mazepa <mazepa@MacBook-Pro-de-Matheus.local>
2020-09-07 23:20:25 -0300

    add role user

--- test/factories/roles.rb
+++ test/factories/roles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :role do
-    name { 'general_manager' }
-    descriprion { 'Diretor geral' }
+    name { 'assistant_administrator' }
+    description { 'Diretor geral' }
   end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     register_number { '123123' }
     sequence(:username) { |n| "usarname#{n}" }
     cpf { CPF.generate(true) }
+    role_id { create(:role).id }
     active { false }
   end
 end
