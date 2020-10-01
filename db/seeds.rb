2673cf388df421fd3816ba77767a27d085c5f38a
paulouskipsk <paulouskipsk@gmail.com>
2020-09-30 23:05:11 -0300

    dropped admin model and respectives migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create_with(name: 'Administrador', password: '123456').find_or_create_by!(email: 'admin@admin.com')
-
 role = Role.create_with(name: 'Administrador geral').find_or_create_by!(identifier: :manager)
 Role.create_with(name: 'Administrador assistente').find_or_create_by!(identifier: :assistant)
 
