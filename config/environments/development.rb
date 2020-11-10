d0a532315327a9d74d10dfc6131a5edf19d11715
paulouski <paulouskipsk@gmail.com>
2020-11-10 16:59:52 -0300

    CD git actions

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,9 +7,9 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.hosts << "tsi-cdms.tk"
-  config.hosts << "www.tsi-cdms.tk"
-  config.hosts << "cdms.tsi.pro.br"
+  config.hosts = 'cdms.tsi.pro.br'
+  config.hosts = 'tsi-cdms.tk'
+  config.hosts = 'www.tsi-cdms.tk'
   
   config.cache_classes = false
 
